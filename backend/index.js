706c843fe941c9c96cbf8578e3c45515a98007e1
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-06-01 13:17:43 -0400

    first push
    push

---
+++ backend/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// Use the cors middleware
+app.use(cors());
+
+//set up connection with DB using our .env info
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+//try to connect to database
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
+  //if successful connection, log it in console
+  console.log('Connected to MySQL');
+});
+
+app.get('/api/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+
+//show a user
+app.head('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+  db.query('SELECT * FROM users WHERE ID=?', [id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.post('/api/users', (req, res) => {
+  const { name, email } = req.body;
+  db.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ id: result.insertId, name, email });
+  });
+});
+
+app.delete('/api/users/:email', (req, res) => {
+  const {email} = req.params;
+  db.query('DELETE FROM users WHERE email = ?', [email], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ id: result.insertId, email });
+  });
+});
+
+//'UPDATE users SET name = ? WHERE email = ?'
+app.put('/api/users', (req, res) => {
+  const { name, email } = req.body;
+  db.query('UPDATE users SET name = ? WHERE email = ?', [name, email], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ id: result.insertId, name, email });
+  });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
